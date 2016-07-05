520c79827d92df090055569fa1d4269aba617a5e
navy <navyboys@gmail.com>
2016-07-05 15:05:25 -0700

    csv output done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+var path = require('path');
 var request = require('request');
 var cheerio = require('cheerio');
 
@@ -5,9 +7,18 @@ request('http://substack.net/images/', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     $ = cheerio.load(body);
     $('tr').each(function() {
-      console.log($(this).find(':first-child').find(':first-child').html());
-      console.log($(this).find(':nth-child(2)').find(':first-child').html());
-      console.log($(this).find(':nth-child(3)').find(':first-child').html());
+      var $permission = $(this).find(':first-child').find(':first-child');
+      var $file = $(this).find(':nth-child(3)').find(':first-child');
+
+      var permission = $permission.text().substring(1).slice(0, -1);
+      var url = $file.attr('href');
+      var type = path.extname($file.text()).substring(1);
+
+      var str = permission + ', ' + url + ', ' + type + '\n';
+
+      fs.appendFile('image.csv', str, function(err) {
+        if (err) throw err;
+      });
     });
   }
 });
